64679794b954a5474c032f96a793db79ac985f64
Lucas <lucassanchezdl@gmail.com>
2022-11-04 03:42:03 -0300

    hero section

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+const btnMenu = document.getElementById("btn-menu");
+const menu = document.getElementById("menu");
+
+const handleBtnMenu = ({ target }) => {
+  if (
+    target.matches(".hamburger-button") ||
+    target.matches(`${".hamburger-button"} *`)
+  ) {
+    btnMenu.classList.toggle("is-active");
+    menu.classList.toggle("nav__active");
+  }
+};
+
+window.swiper = new Swiper({
+  el: ".slider__contenedor",
+  slideClass: "slider__slide",
+  createElements: true,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  pagination: true,
+  navigation: true,
+});
+
+document.addEventListener("click", handleBtnMenu);
