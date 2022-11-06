abf625bc1300de51d1f6ca97f67d56fc3f39ff55
Lucas <lucassanchezdl@gmail.com>
2022-11-06 01:50:11 -0300

    productos destacados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
+import { products } from "./data.js";
+
 const btnMenu = document.getElementById("btn-menu");
 const menu = document.getElementById("menu");
+const featuredContainer = document.getElementById("featured");
 
 const handleBtnMenu = ({ target }) => {
   if (
@@ -23,4 +26,37 @@ window.swiper = new Swiper({
   navigation: true,
 });
 
+const renderFeaturedProducts = () => {
+  const featuredProducts = products
+    .filter((product) => product.featured === true)
+    .map(createFeaturedProduct)
+    .join("");
+  featuredContainer.innerHTML = featuredProducts;
+};
+
+const createFeaturedProduct = (product) => {
+  const { id, brand, model, version, price, images } = product;
+  return `
+  
+    <div class="card">
+      <i class="card__icon fa-regular fa-heart"></i>
+      <div class="card__img-box">
+        <img class="card__img" src="${
+          images[0]
+        }" alt="imagen de la zapatilla" />
+      </div>
+      <div class="card__info">
+        <a  href="./assets/pages/tienda.html">
+          <span class="card__title">
+          ${brand} ${model} ${version ? version : ""}
+          </span>
+        </a>
+        <span class="card__price">$${price}</span>
+      </div>
+  </div>
+  
+  `;
+};
+
 document.addEventListener("click", handleBtnMenu);
+document.addEventListener("DOMContentLoaded", renderFeaturedProducts);
